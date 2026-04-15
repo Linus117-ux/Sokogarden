e7f482cb6a72b0d46c4f431d321dd70fa013d335
Linus117-ux <linusmuthamakaunga@gmail.com>
2026-04-15 23:39:18 +0300

    added updates

--- app/src/main/java/com/example/sokogarden/MainActivity.kt
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.sokogarden
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +34,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, Signin::class.java)
             startActivity(intent)
         }
+//        find recycleveiw and progress bar
+        val recyclerVeiw = findViewById<RecyclerView>(R.id.recyclerveiw)
+        val progressbar = findViewById<ProgressBar>(R.id.progressbar)
+
+
+        //Specify the api url endpoint for fetching products
+        val url = "https://linushiggs.alwaysdata.net/api/getproduct"
+
+//        import helper class
+        val helper = ApiHelper(applicationContext)
+//       Inside helper class access function loadproducts
+        helper.loadProducts(url , recyclerVeiw , progressbar)
     }
 
 }
